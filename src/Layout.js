183aecd44b56d106a669fe5a9f0cceda17443e2b
michal sagi <michaldinner@gmail.com>
2022-10-30 18:36:37 +0200

    Header

---
+++ src/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './components/Header'
+
+function Layout() {
+    return (
+        <div>
+            <Header />
+        </div>
+    )
+}
+
+export default Layout
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import styles from "./style.module.css"
+import userImg from '../../Assets/Images/user.jpg'
+
+
+function Header() {
+    return (
+        <div className={styles.headerContainer}>
+            <div className={styles.headerInnerWidth}>
+                <div className={styles.workshopHeader}>
+                    <>
+                        <img
+                            src={userImg}
+                            alt="user"
+                            className={styles.headerUser}
+                        />
+                        <div className={styles.titleHeader}>{"דנה לוי"}</div>
+                    </>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header
